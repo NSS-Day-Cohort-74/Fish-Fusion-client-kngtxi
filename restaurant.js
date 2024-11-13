a59503a2e18faf75983c567278a4e8090c91496e
Kai Swain <kaiferseain@icloud.com>
2024-11-13 13:57:06 -0600

    .map implemented in restaurant.js

--- restaurant.js
+++ restaurant.js
@@ -3,14 +3,15 @@ const { mongerInventory } = require("./fishMonger.js")
 const fishMenu = (dailyPriceLimit) => {
     const chefPurchasesArray = mongerInventory(dailyPriceLimit);
    
-   
-     
-  for (const fishObj of chefPurchasesArray) {
-      itemHTML += `\n\t<h2>${fishObj.species}</h2>
-      <section class="menu__item">${fishObj.species} Soup</section>
-      <section class="menu__item">${fishObj.species} Sandwich</section>
-      <section class="menu__item">Grilled ${fishObj.species}</section>`
-  }
+
+    let itemHTML = chefPurchasesArray.map((fishObj) => 
+        `\n\t<h2>${fishObj.species}</h2>
+        <section class="menu__item">${fishObj.species} Soup</section>
+        <section class="menu__item">${fishObj.species} Sandwich</section>
+        <section class="menu__item">Grilled ${fishObj.species}</section>` 
+    ).join('')
+        
+       
 
         const menuHTML = `<h1>Menu</h1>\n<article class="menu">\n${itemHTML}\n</article>`
         
